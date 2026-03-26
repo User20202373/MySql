18f1dbac2f27700afce14c97bdfc9509b96ff195
최원종 <chl6698@naver.com>
2026-03-26 16:47:09 +0900

    first commit

---
+++ class4-1.sql
@@ -0,0 +1,40 @@
+-- 데이터 베이스 구축하기
+CREATE DATABASE school;
+
+-- 어떤 데이터 베이스를 사용하겠다.
+use school;
+
+-- 테이블 생성
+
+-- 1. 학생 테이블
+CREATE TABLE student(
+	student_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    grade INT NOT NULL,
+    major VARCHAR(50) NOT NULL,
+    phone VARCHAR(20) 
+);
+
+select * from student;
+
+-- 2. 과목 테이블
+CREATE TABLE subject(
+	subject_id INT PRIMARY KEY,
+    subject_name VARCHAR(50) NOT NULL,
+    credit INT NOT NULL,
+    professor VARCHAR(30) NOT NULL
+);
+
+-- 수강 테이블(stuent 테이블 subject 테이블 연결) -> FK
+CREATE TABLE enrollment(
+	enrollment_id INT PRIMARY KEY,
+    student_id INT NOT NULL, -- 일반적으로 FK를 선언시 not null로 설정함 
+    subject_id int not null,
+    score 		DECiMAL(5, 2),
+    enrolleD_AT DATE nOT null,
+    foreign KEY(student_Id) reFERENCEs STUdent(student_ID),-- 참조하는 테이블 대상이 student
+	Foreign key(subject_id) references subject(subject_ID)
+);
+
+show tables;
+
